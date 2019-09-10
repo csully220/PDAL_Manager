efcae148e8e634485ab6155c871ef219aa392373
Unknown <colinsully@msn.com>
2019-09-10 12:47:30 -0500

    Add files

---
+++ compare_revisions_one_folder.py
@@ -0,0 +1,45 @@
+import os
+import re
+import sys
+
+import tkinter as tk
+from tkinter import filedialog
+import ntpath
+
+root = tk.Tk()
+root.withdraw()
+
+workdir = filedialog.askdirectory()
+
+print("Find drawings with revisions")
+os.chdir(workdir)
+files = os.listdir()
+
+
+def main():
+    counter = 0;
+    for file in files:
+        try:
+            fsp = file.split('.')
+            dwg_ext = fsp[-1]
+            dwg_fn = fsp[0].upper().split('_')
+            dwg_no = dwg_fn[:-1]
+            dwg_rev = dwg_fn[-1]
+            for cfile in files:
+                cfsp = cfile.split('.')
+                cdwg_ext = cfsp[-1]
+                cdwg_fn = cfsp[0].upper().split('_')
+                cdwg_no = cdwg_fn[:-1]
+                cdwg_rev = cdwg_fn[-1]
+                if dwg_no == cdwg_no and dwg_ext == cdwg_ext and cdwg_rev != dwg_rev:
+                    print(str(dwg_no[0]) + "." + str(dwg_ext) + " has revisions " + dwg_rev + " and " + cdwg_rev)
+                    continue
+
+        except Exception as e:
+            #print(repr(e) + '       ' + file)
+            continue
+                     
+    #print('Renamed ' + str(counter) + ' files in ' + workdir)
+    input("Press Enter to exit...")
+
+main()
---
+++ fix_filenames.py
@@ -0,0 +1,98 @@
+import os
+import re
+import sys
+
+import tkinter as tk
+from tkinter import filedialog
+import ntpath
+
+root = tk.Tk()
+root.withdraw()
+
+workdir = filedialog.askdirectory()
+
+# CAE dwg numbers defined as ALPHA PREFIX | SERIAL NUM | TAB NUM | TYPE CODE | FAMILY GROUP
+# File names we have are something like ALPHA PREFIX | SERIAL | TAB NUM | TYPE (FD or PL) | REVISION but are not consistent
+
+# Desired format is SERIAL NUM | TAB NUM | TYPE | REV
+# ex. ma399477_10_A_pl.pdf will read MA399477_10_PL_A.pdf after running this script.
+
+# Do not remove the "r" from the front of the string and no trailing backslash. 
+# Enter just like this: r'C:\Users\You\path\to\files'
+
+print("CAE drawing file renaming tool")
+print("Select directory containing filenames to be fixed")
+#text = input("Enter directory: ")
+valid_prefixes = ['CA','MA','PS','WD','TS','UD','MD','FP','ED','CD','PD']
+valid_filetypes = ['pdf','dgn','dwg','dxf','xls','xlsx']
+
+os.chdir(workdir)
+print(os.getcwd())
+input("Press Enter to continue")
+files = os.listdir()
+
+def main():
+    counter = 0;
+    for file in files:
+        try:
+            fsp = file.split('.')
+            dwg_no = fsp[0].upper()
+            dwg_sp = dwg_no.split('_')
+
+            # FILETYPE
+            fn_ext = None
+
+            if fsp[1].lower() in valid_filetypes:
+                fn_ext = fsp[1].lower()
+            else:
+                raise Exception( 'Filetype not valid' )
+        
+            # ALPHA PREFIX
+
+            base = None            
+            alpha = dwg_sp[0][:2]
+            if alpha in valid_prefixes:
+                base = dwg_sp[0]
+            else:
+                raise Exception('Alpha prefix not valid')
+     
+            # TAB NUMBER
+         
+            tab = None
+            for s in dwg_sp[1:]:
+                if re.search("\d{2}", s):
+                    tab = s
+                
+            # TYPE (PARTS LIST or FD)
+            type = None
+            for s in dwg_sp[1:]:
+                if s == 'FD' or s == 'PL':
+                    type = s
+                    
+            # REVISION
+            rev = None
+            for s in dwg_sp[1:]:
+                if (len(s) == 1 or len(s) == 2) and (re.search("[A-Z]", s) or re.search("[A-Z]{2}", s) or s == '-') and (s != 'FD' and s != 'PL'):
+                    rev = s
+                elif len(s) == 1 and s == '0':
+                    rev = '-'
+ 
+            if(base and tab and type and rev and fn_ext):
+                new_fn = base + '_' + tab + '_' + type + '_' + rev + '.' + fn_ext
+                #print(new_fn)
+            try:
+                if new_fn != file:
+                    os.rename(file, new_fn)
+                    #print(file + '   -->   ' + new_fn)
+                    counter += 1
+            except:
+                continue
+
+        except Exception as e:
+            print(repr(e) + '       ' + file)
+            continue
+                     
+    print('Renamed ' + str(counter) + ' files in ' + workdir)
+    input("Press Enter to exit...")
+
+main()
